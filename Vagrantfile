1239d9aaa1b3d146e89598dcba20f777d0cd85c3
everysick <s.wakeup31@gmail.com>
2017-07-18 20:01:57 +0900

    Add template files

---
+++ Vagrantfile
@@ -0,0 +1,9 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/xenial64"
+  config.vm.provision 'shell', path: 'bootstrap.sh', privileged: false
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.memory = 2048
+    vb.cpus = 2
+  end
+end
---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+
+#!/usr/bin/env bash
+
+test -f ~/bootstrapped && exit
+
+cd ~
+
+sudo add-apt-repository -y ppa:kelleyk/emacs
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install emacs25-nox tmux git zsh make
+
+mkdir src
+cd src
+git clone https://github.com/Everysick/dotfiles.git
+cd dotfiles
+make
+
+sudo chsh -s /usr/bin/zsh ubuntu
+
+date > ~/bootstrapped
