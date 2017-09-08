99d11f68901505ffccc1fddb7cbef7a68bc1c6e5
everysick <s.wakeup31@gmail.com>
2017-09-08 22:53:28 +0900

    Add install line of golang

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,12 @@ test -f ~/bootstrapped && exit
 
 cd ~
 
+sudo add-apt-repository -y ppa:longsleep/golang-backports
 sudo add-apt-repository -y ppa:kelleyk/emacs
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install emacs25-nox tmux git zsh make
+sudo apt-get install golang-go
 
 mkdir src
 cd src
